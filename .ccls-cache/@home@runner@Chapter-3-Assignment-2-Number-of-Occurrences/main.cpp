90dcdccf90274c09f303bc2e6532a44f7a4020e1
harshita <71144962+harshita-ceh@users.noreply.github.com>
2022-02-28 13:07:52 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Chapter-3-Assignment-2-Number-of-Occurrences/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int Occurrences(int arr[],int n,int x){
+  int res = 0;
+    for (int i=0; i<n; i++)
+        if (x == arr[i])
+          res++;
+    return res;
+}
+int main() {
+  int arr[] = {1, 2, 2, 2, 2, 3, 4, 7 ,8 ,8 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 2;
+    cout << Occurrences(arr, n, x);
+    return 0;
+}
